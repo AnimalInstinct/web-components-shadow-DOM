4f36b71a6dca0ed284d19ddc9b4666956ff017e2
AnimalInstinct <animalinstnct@protonmail.com>
2022-09-19 10:50:35 +0700

    Shadow DOM

--- tooltip.js
+++ tooltip.js
@@ -2,26 +2,43 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: grey;
+                    color: white;
+                    position: absolute;
+                }
+
+                p {
+                    position: relative;
+                }
+            </style>
+            <slot>Default tooltip text</slot>
+            <span>(?)</span>
+        `;
     }
     
     connectedCallback() {
+        this._tooltipText = this.getAttribute('text');
         console.log('Connected callback');
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
+        const tooltipIcon = this.shadowRoot.querySelector('span')
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
-        this.appendChild(tooltipIcon);
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+        this.shadowRoot.appendChild(tooltipIcon);
     }
 
     _showTooltip() {
         console.log('Show tooltip');
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = "This is the tooltip text!"
-        this.appendChild(this._tooltipContainer);
+        this._tooltipContainer.textContent = this._tooltipText || "Dummy"
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
         console.log('Hide Tooltip');
-        this._
+        this.shadowRoot.removeChild(this._tooltipContainer)
     }
 
     disconnectedCallback() {
