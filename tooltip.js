67666be8a718d515f8e8c1de4eb95deb8fd5f928
AnimalInstinct <animalinstnct@protonmail.com>
2022-09-21 10:53:06 +0700

    Render method, change state, reactivity

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,10 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        this._tooltipContainer;
+        this._tooltipIcon;
+        this._tooltipVisible = false;
+        this._tooltipText = "Default tooltip text from the constructor";
+        console.log('Constructor::tooltipText:: ', this._tooltipText);
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.innerHTML = `
         <style>
@@ -9,28 +12,70 @@ class Tooltip extends HTMLElement {
         </style>
         <slot>Default tooltip text</slot>
         <span>?</span>
+        <a id="test" href="#">Click me to set new tooltip text</a>
         `;
     }
     
     connectedCallback() {
+        console.log('Component connected');
         this._tooltipText = this.getAttribute('text');
-        const tooltipIcon = this.shadowRoot.querySelector('span')
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
-        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
-        this.shadowRoot.appendChild(tooltipIcon);
-    }
+        this._tooltipIcon = this.shadowRoot.querySelector('span')
+        console.log('connectedCallback::tooltipText:: ', this._tooltipText);
 
-    _showTooltip() {
-        this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = this._tooltipText || "Dummy"
-        this.shadowRoot.appendChild(this._tooltipContainer);
-    }
+        this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+        this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+        this.shadowRoot.appendChild(this._tooltipIcon);
 
-    _hideTooltip() {
-        this.shadowRoot.removeChild(this._tooltipContainer)
+        const testButton = this.shadowRoot.querySelector('#test')
+        testButton.addEventListener('click', this.changeHandler.bind(this))
     }
 
     disconnectedCallback() {
+        console.log('Component disconnected');
+    }
+
+    changeHandler() {
+        this._tooltipText = 'New tooltip text changed!'
+        this._render();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) {
+            return;
+        }
+
+        if (name === 'text') {
+            console.log('attributeChangedCallback::Tooltip text changed to::', newValue);
+            this._tooltipText = newValue;
+        }
+    }
+
+    static get observedAttributes() {
+        return ['text']
+    }
+
+    _showTooltip() {
+        this._tooltipVisible = true;
+        this._render();
+    }
+
+    _hideTooltip() {
+        this._tooltipVisible = false;
+        this._render();
+    }
+
+    _render() {
+        console.log('Render function');
+        let tooltipContainer;
+
+        if (this._tooltipVisible) {
+            tooltipContainer = document.createElement('div');
+            tooltipContainer.textContent = this._tooltipText || "Dummy"
+            this.shadowRoot.appendChild(tooltipContainer);
+        } else {
+            tooltipContainer = this.shadowRoot.querySelector('div')
+            tooltipContainer && this.shadowRoot.removeChild(tooltipContainer)
+        }
     }
 }
 
